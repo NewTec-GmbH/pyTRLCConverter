2e650a5dc7bc1777360f0efd524efc5ce09a4c5a
BlueAndi <web@blue-andi.de>
2026-03-20 00:14:04 +0100

    Support for multiple documentation versions on GitHub pages added.

--- tools/deployDoc/source/conf.py
+++ tools/deployDoc/source/conf.py
@@ -24,6 +24,7 @@
 import os
 import shutil
 import fnmatch
+import json
 
 from typing import Any
 from urllib.parse import urlparse
@@ -37,7 +38,7 @@ from sphinx.errors import ConfigError
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'pyTRLCConverter'
-copyright = '2025, NewTec GmbH'
+copyright = '2024 - 2026, NewTec GmbH'
 author = 'NewTec GmbH'
 
 # -- General configuration ---------------------------------------------------
@@ -73,6 +74,7 @@ myst_heading_anchors = 6
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
+html_js_files = ['version_selector.js']
 
 # Copy favorite icon to static path.
 html_favicon = '../../../doc/images/favicon.ico'
@@ -83,6 +85,62 @@ html_logo = '../../../doc/images/NewTec_Logo.png'
 # PlantUML is called OS depended and the java jar file is provided by environment variable.
 plantuml_env = os.getenv('PLANTUML')
 plantuml = []
+
+
+def _build_version_links() -> tuple[str, list[tuple[str, str]]]:
+    """Build documentation version selector entries for the RTD theme.
+
+    Environment parameters:
+        DOCS_VERSION: Current docs version shown as active entry.
+            Falls back to GITHUB_REF_NAME and finally ``local``.
+        DOCS_VERSIONS: Comma-separated list of versions shown in the selector.
+            Defaults to ``latest,unstable``.
+        DOCS_LATEST_TARGET: Version folder used for the ``latest`` selector entry.
+            Defaults to ``latest``.
+        DOCS_BASE_PATH: Base URL path for generated links.
+            If not set, it is derived from GITHUB_REPOSITORY.
+
+    Returns:
+        tuple[str, list[tuple[str, str]]]:
+            Current version and a list of ``(label, url)`` selector entries.
+    """
+    current_version = os.getenv('DOCS_VERSION') or os.getenv('GITHUB_REF_NAME') or 'local'
+    configured_versions = os.getenv('DOCS_VERSIONS', 'unstable')
+    latest_target = os.getenv('DOCS_LATEST_TARGET', 'latest').strip() or 'latest'
+    versions = [version.strip() for version in configured_versions.split(',') if version.strip()]
+
+    if current_version not in versions:
+        versions.insert(0, current_version)
+
+    docs_base_path = os.getenv('DOCS_BASE_PATH', '')
+    if not docs_base_path:
+        repository = os.getenv('GITHUB_REPOSITORY', '')
+        if '/' in repository:
+            docs_base_path = f"/{repository.split('/', maxsplit=1)[1]}"
+
+    docs_base_path = docs_base_path.rstrip('/')
+    version_links = []
+    for version in versions:
+        if docs_base_path:
+            version_links.append((version, f"{docs_base_path}/{version}/"))
+        else:
+            version_links.append((version, f"/{version}/"))
+
+    if docs_base_path:
+        latest_url = f"{docs_base_path}/{latest_target}/"
+    else:
+        latest_url = f"/{latest_target}/"
+
+    version_links.insert(0, ('latest', latest_url))
+
+    return current_version, version_links
+
+current_version, versions_for_selector = _build_version_links()
+html_context = {
+    'current_version': current_version,
+    'version': current_version,
+    'versions': versions_for_selector
+}
 
 # Classes **********************************************************************
 
@@ -111,7 +169,28 @@ def setup(app: Any) -> None:
     Args:
         app (Any): The sphinx application.
     """
+    app.connect('builder-inited', write_version_selector_data)
     app.connect('builder-inited', copy_files)
+
+
+def write_version_selector_data(app: Any) -> None:
+    """Create selector data consumed by local version selector JavaScript.
+
+    Args:
+        app (Any): The sphinx application.
+    """
+    static_dir = os.path.join(os.path.dirname(__file__), '_static')
+    selector_file = os.path.join(static_dir, 'version_selector_data.js')
+
+    selector_data = {
+        'current': current_version,
+        'versions': [{'label': slug, 'url': url} for slug, url in versions_for_selector]
+    }
+
+    with open(selector_file, 'w', encoding='utf-8') as file:
+        file.write(
+            f"window.PYTRLC_DOCS_VERSION_SELECTOR = {json.dumps(selector_data)};\n"
+        )
 
 def copy_files(app: Any) -> None:
     """Copy files to the output directory.
